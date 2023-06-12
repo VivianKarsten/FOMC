df04639b7eb2edfa5a32e23b6a9775a0b3f941f4
Timing Liu <39947942+liutiming@users.noreply.github.com>
2023-06-12 15:39:49 +0000

    oulined code

--- main.py
+++ main.py
@@ -13,21 +13,25 @@ class TradingFOMCAnnouncementsSummaryEconomicProjections(QCAlgorithm):
         self.SetStartDate(2007, 1, 1)
         self.SetCash(100000)
         
-        self.symbol = self.AddEquity("SPY", Resolution.Minute).Symbol
+        self.symbol_2yr = self.AddEquity("SPY", Resolution.Minute).Symbol # TODO work out which symbol should be put here for 2 year treasury  
+        self.symbol_1yr = self.AddEquity("SPY", Resolution.Minute).Symbol# TODO work out which symbol should be put here for 1 year treasury
 
-        # csv_string_file = self.Download('data.quantpedia.com/backtesting_data/economic/fed_summary_economic_projections.csv')
-        # dates = csv_string_file.split('\r\n')
-        # dates = [datetime.strptime(x, "%Y-%m-%d") for x in dates]
+        self.liquidate_at_the_end = True
         
-        self.liquidate_next_day = True
+        announcement_time = datetime.strptime("2023-06-14-", "%Y-%m-%d") # TODO work out what should be put here - considering the timezone as well
+
+        self.signal = "" # TODO need to know what signal it is 
+        self.signal_threshold = 0 # TODO need to test the signal threshold
+        self.percent_1yr = 0 # TODO need to test out what this is 
+        self.percent_2yr = - (1 - self.percent_1yr)
         
-        self.Schedule.On(self.DateRules.On(dates), self.TimeRules.BeforeMarketClose(self.symbol, 1), self.Trade)
+        self.Schedule.On(self.DateRules.On(announcement_time), self.TimeRules.BeforeMarketClose(self.symbol, 1), self.Trade)
         self.Schedule.On(self.DateRules.EveryDay(self.symbol), self.TimeRules.BeforeMarketClose(self.symbol, 1), self.Rebalance)
     
     def Trade(self):
-        if not self.Portfolio[self.symbol].IsLong:
-            self.SetHoldings(self.symbol, 1)
-            self.liquidate_next_day = True        
+        self.SetHoldings(self.symbol_1yr, self.percent_1yr)
+        self.SetHoldings(self.symbol, - self.percent_2yr)
+        self.liquidate_at_the_end = True      
 
     def Rebalance(self):
         if self.liquidate_next_day:
